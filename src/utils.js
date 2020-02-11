527a6bb4cfa4b6cc3a07e914739945fdf220e031
Amorem <dev@amorem.com>
2020-02-11 23:10:06 +0100

    secretMail (not finished)

--- src/utils.js
+++ src/utils.js
@@ -4,3 +4,14 @@ export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
 };
+
+export const sendMail = email => null;
+
+export const sendSecretMail = (adress, secret) => {
+  const email = {
+    from: "support@prismagram.com",
+    to: adress,
+    subject: "Login Secret for Prismgram 🔒",
+    html: `Hello! Your login secret is ${secret}.<br/>Copy paste on the app/website to login`
+  };
+};

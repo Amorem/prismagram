6bee421a5eb6545b654584dacd37eda926d0835d
Amorem <dev@amorem.com>
2020-02-12 22:30:15 +0100

    sendgrid OK

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,9 @@
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport";
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 import { adjectives, nouns } from "./words";
 
 export const generateSecret = () => {
@@ -5,7 +11,16 @@ export const generateSecret = () => {
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
 };
 
-export const sendMail = email => null;
+const sendMail = email => {
+  const options = {
+    auth: {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD
+    }
+  };
+  const client = nodemailer.createTransport(sgTransport(options));
+  return client.sendMail(email);
+};
 
 export const sendSecretMail = (adress, secret) => {
   const email = {
@@ -14,4 +29,5 @@ export const sendSecretMail = (adress, secret) => {
     subject: "Login Secret for Prismgram 🔒",
     html: `Hello! Your login secret is ${secret}.<br/>Copy paste on the app/website to login`
   };
+  return sendMail(email);
 };

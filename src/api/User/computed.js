a8052a01af4ea6a66ebee6f42c71d4845a8399c8
Amorem <dev@amorem.com>
2020-02-19 15:05:44 +0100

    isLiked

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -4,23 +4,35 @@ export default {
   User: {
     fullname: parent => {
       return `${parent.firstName} ${parent.lastname}`;
+    },
+
+    isFollowing: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      try {
+        return await prisma.$exists.user({
+          where: {
+            AND: [{ id: user.id }, { following_some: { id: parentId } }]
+          }
+        });
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+    isSelf: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return user.id === parent.id;
     }
   },
-  amIFollowing: async (parent, _, { request }) => {
-    const { user } = request;
-    const { id: parentId } = parent;
-    try {
-      return await prisma.$exists.user({
-        where: { AND: [{ id: user.id }, { following_some: { id: parentId } }] }
+  Post: {
+    isLiked: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id } = parent;
+      return prisma.$exists.like({
+        AND: [{ user: { id: user.id } }, { post: { id } }]
       });
-    } catch (error) {
-      console.log(error);
-      return false;
     }
-  },
-  itsMe: (parent, _, { request }) => {
-    const { user } = request;
-    const { id: parentId } = parent;
-    return user.id === parent.id;
   }
 };
